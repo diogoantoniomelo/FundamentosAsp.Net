723cec3aff20c49d5a054cb0e9059e0d12f1872f
Diogo Melo <diogo.antoniomelo@gmail.com>
2023-08-09 21:29:59 -0300

    Fundamentos Asp.Net

--- Configuration.cs
+++ Configuration.cs
@@ -2,9 +2,9 @@
 {
     public static class Configuration
     {
-        public static string JwtKey  = "ZmVkYWY3ZDg4NjNiNDhlMTk3YjkyODdkNDkyYjcwOGU=";
+        public static string JwtKey  = "";
         public static string ApiKeyName = "api_key";
-        public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
+        public static string ApiKey = "";
         public static SmtpConfiguration Smtp = new();
         public class SmtpConfiguration
         {
